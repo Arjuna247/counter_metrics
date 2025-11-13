d66adefe5fe0d8050bf2288772142c0c95652977
Harish Ram <harishram2407@gmail.com>
2025-11-13 11:36:42 +0530

    updated backend code

--- backend.js
+++ backend.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.get('http://localhost:3000/api/data', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-})
-
-app.listen(3000, () => {
-    console.log('Backend server is running on http://localhost:3000');
+app.use(cors());
+app.use(express.json());
+app.post('/save-count', (req, res) => {
+    console.log('Button count:', req.body.count);
+    res.json({ success:true, storedCount: req.body.count });
 });
+
+
+app.listen(3000, () => 
+    console.log('Backend server is running on 3000'));
