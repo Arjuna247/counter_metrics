ffbd9413a52077d1012d3a13ab6056efdca87a81
Your Name <arjunmmn611@gmail.com>
2025-11-13 12:57:04 +0530

    updated js

--- script.js
+++ script.js
@@ -6,5 +6,13 @@ button.addEventListener("click", () => {
   count++;
   countDisplay.textContent = count;
 
+  // Send updated count to backend
+  fetch("http://localhost:3000/save-count", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ count: count })
+  })
+  .then(res => res.json())
+  .then(data => console.log("Server response:", data))
+  .catch(err => console.error(err));
 });
-
